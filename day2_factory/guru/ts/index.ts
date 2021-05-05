7664f2c5579b26d37b554758c1884762aeea0847
Lai-YT <381xvmvbib@gmail.com>
2021-05-05 20:39:03 +0800

    factory: ts

---
+++ day2_factory/guru/ts/index.ts
@@ -0,0 +1,25 @@
+import * as ctr from "./creator"
+
+// The client code works with an instance of a concrete creator, albeit through
+// its base interface. As long as the client keeps working with the creator via
+// the base interface, you can pass it any creator's subclass.
+function clientCode(creator: ctr.Creator): void {
+    console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
+    console.log(creator.someOperation());
+}
+
+// The Application picks a creator's type depending on the configuration or
+// environment.
+console.log('App: Launched with the ConcreteCreator1.');
+clientCode(new ctr.ConcreteCreator1());
+console.log('');
+
+console.log('App: Launched with the ConcreteCreator2.');
+clientCode(new ctr.ConcreteCreator2());
+// App: Launched with the ConcreteCreator1.
+// Client: I'm not aware of the creator's class, but it still works.
+// Creator: The same creator's code has just worked with {Result of the ConcreteProduct1} 
+
+// App: Launched with the ConcreteCreator2.
+// Client: I'm not aware of the creator's class, but it still works.
+// Creator: The same creator's code has just worked with {Result of the ConcreteProduct2}
---
+++ day2_factory/guru/ts/product.ts
@@ -0,0 +1,20 @@
+// The Product interface declares the operations that all concrete products must
+// implement.
+export interface Product {
+	operation(): string;
+}
+
+
+// Concrete Products provide various implementations of the Product interface.
+export class ConcreteProduct1 implements Product {
+	public operation(): string {
+		return '{Result of the ConcreteProduct1}';
+	}
+}
+
+export class ConcreteProduct2 implements Product {
+	public operation(): string {
+		return '{Result of the ConcreteProduct2}';
+	}
+}
+
